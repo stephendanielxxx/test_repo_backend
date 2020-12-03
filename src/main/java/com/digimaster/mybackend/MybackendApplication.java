7347275998f5fc3fa68052b4c898df02726d70af
Stephen <stephendanielxxx@gmail.com>
2020-12-03 12:52:07 +0700

    add logging

--- src/main/java/com/digimaster/mybackend/MybackendApplication.java
+++ src/main/java/com/digimaster/mybackend/MybackendApplication.java
@@ -3,11 +3,19 @@ package com.digimaster.mybackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class MybackendApplication {
-
+	static final Logger logger = 
+	        LoggerFactory.getLogger(MybackendApplication.class);
+	
 	public static void main(String[] args) {
+		logger.info("Before Starting application");
 		SpringApplication.run(MybackendApplication.class, args);
+		logger.debug("Starting my application in debug with {} arguments", args.length);
+		logger.info("Starting my application with {} arguments.", args.length);
 	}
 
 }
